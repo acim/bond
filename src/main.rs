155d2809a5d44e63a70dd81589fc202eabc49799
Boban Acimovic <boban.acimovic@gmail.com>
2021-02-06 14:12:48 +0100

    try crd

--- src/main.rs
+++ src/main.rs
@@ -1,175 +1,133 @@
-// Copyright 2020 Boban Acimovic
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
-//!
-#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
-
 #[macro_use]
 extern crate log;
-use clap::Clap;
-use futures::{stream, StreamExt, TryStreamExt};
-use k8s_openapi::api::core::v1::Secret;
+use color_eyre::{Report, Result};
+use futures::StreamExt;
+use k8s_openapi::{
+    api::core::v1::ConfigMap,
+    apimachinery::pkg::apis::meta::v1::{ObjectMeta, OwnerReference},
+};
 use kube::{
-    api::{Api, ListParams},
-    Client,
+    api::{ListParams, Meta, Patch, PatchParams},
+    Api, Client, CustomResource,
 };
-use kube_runtime::{
-    watcher,
-    watcher::Event::{Applied, Deleted, Restarted},
-};
-use serde_json::json;
-use std::io::Error;
-use warp::Filter;
+use kube_runtime::controller::{Context, Controller, ReconcilerAction};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use snafu::{Backtrace, OptionExt, ResultExt, Snafu};
+use std::collections::BTreeMap;
+use tokio::time::Duration;
 
-mod client;
-
-const LABEL: &str = "app.kubernetes.io/managed-by";
-
-/// Kubernetes secrets replication across namespaces
-#[derive(Clap, Debug)]
-#[clap(name = "bond")]
-struct Opt {
-    /// Sets a custom config file
-    #[clap(short, long, default_value = "bond")]
-    config: String,
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("Failed to create ConfigMap: {}", source))]
+    ConfigMapCreationFailed {
+        source: kube::Error,
+        backtrace: Backtrace,
+    },
+    MissingObjectKey {
+        name: &'static str,
+        backtrace: Backtrace,
+    },
 }
 
-#[derive(Debug, serde_derive::Serialize, serde_derive::Deserialize)]
-struct SecretReplica {
-    source: String,
-    destination: Vec<String>,
+#[derive(CustomResource, Debug, Clone, Deserialize, Serialize, JsonSchema)]
+#[kube(group = "nullable.se", version = "v1", kind = "ConfigMapGenerator")]
+#[kube(shortname = "cmg", namespaced)]
+struct ConfigMapGeneratorSpec {
+    content: String,
 }
 
-#[derive(Default, Debug, serde_derive::Serialize, serde_derive::Deserialize)]
-struct Config {
-    replica: Vec<SecretReplica>,
+fn object_to_owner_reference<K: Meta>(meta: ObjectMeta) -> Result<OwnerReference, Error> {
+    Ok(OwnerReference {
+        api_version: K::API_VERSION.to_string(),
+        kind: K::KIND.to_string(),
+        name: meta.name.context(MissingObjectKey {
+            name: ".metadata.name",
+        })?,
+        uid: meta.uid.context(MissingObjectKey {
+            name: ".metadata.backtrace",
+        })?,
+        ..OwnerReference::default()
+    })
+}
+
+/// Controller triggers this whenever our main object or our children changed
+async fn reconcile(
+    generator: ConfigMapGenerator,
+    ctx: Context<Data>,
+) -> Result<ReconcilerAction, Error> {
+    let client = ctx.get_ref().client.clone();
+
+    let mut contents = BTreeMap::new();
+    contents.insert("content".to_string(), generator.spec.content);
+    let cm = ConfigMap {
+        metadata: ObjectMeta {
+            name: generator.metadata.name.clone(),
+            owner_references: Some(vec![OwnerReference {
+                controller: Some(true),
+                ..object_to_owner_reference::<ConfigMapGenerator>(generator.metadata.clone())?
+            }]),
+            ..ObjectMeta::default()
+        },
+        data: Some(contents),
+        ..Default::default()
+    };
+    let cm_api = Api::<ConfigMap>::namespaced(
+        client.clone(),
+        generator
+            .metadata
+            .namespace
+            .as_ref()
+            .context(MissingObjectKey {
+                name: ".metadata.namespace",
+            })?,
+    );
+    cm_api
+        .patch(
+            cm.metadata.name.as_ref().context(MissingObjectKey {
+                name: ".metadata.name",
+            })?,
+            &PatchParams::apply("configmapgenerator.kube-rt.nullable.se"),
+            &Patch::Apply(&cm),
+        )
+        .await
+        .context(ConfigMapCreationFailed)?;
+    Ok(ReconcilerAction {
+        requeue_after: Some(Duration::from_secs(300)),
+    })
+}
+
+/// The controller triggers this on reconcile errors
+fn error_policy(_error: &Error, _ctx: Context<Data>) -> ReconcilerAction {
+    ReconcilerAction {
+        requeue_after: Some(Duration::from_secs(1)),
+    }
+}
+
+// Data we want access to in error/reconcile calls
+struct Data {
+    client: Client,
 }
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    std::env::set_var("RUST_LOG", "info,kube=debug");
+async fn main() -> Result<()> {
+    std::env::set_var("RUST_LOG", "info,kube-runtime=debug,kube=debug");
     env_logger::init();
-
-    let opt: &Opt = &Opt::parse();
-    info!("Options: {:#?}", opt);
-
-    let cfg: Config = confy::load(opt.config.as_str(), None).unwrap();
-    info!("Config: {:#?}", cfg);
-
-    tokio::spawn(async { serve().await });
-
     let client = Client::try_default().await?;
 
-    let mut nss = Vec::with_capacity(cfg.replica.len());
-    let lp = ListParams::default().allow_bookmarks();
-    for sr in cfg.replica.iter() {
-        let (ns, _) = client::split_full_name(&sr.source);
-        let a: Api<Secret> = Api::namespaced(client.clone(), ns);
-        nss.push(watcher(a, lp.clone()).boxed());
-    }
+    let cmgs = Api::<ConfigMapGenerator>::all(client.clone());
+    let cms = Api::<ConfigMap>::all(client.clone());
 
-    let mut w = stream::select_all(nss);
-    let mut api = client::KubeApi::new(client);
-
-    // let mut w = watcher(cms, lp).boxed();
-    while let Some(event) = w.try_next().await? {
-        match event {
-            Applied(x) => info!("Applied: {}", client::full_name(&x)),
-            Deleted(x) => info!("Deleted: {}", client::full_name(&x)),
-            Restarted(x) => {
-                for y in x.iter() {
-                    info!("Restarted: {}", client::full_name(&y));
-
-                    for i in cfg.replica.iter() {
-                        if client::full_name(&y) == i.source {
-                            for d in i.destination.iter() {
-                                info!("Getting destination secret {}", &d);
-                                match api.get(&d).await {
-                                    Ok(s) => {
-                                        info!("Found {}", client::full_name(&s));
-                                        if y.data == s.data {
-                                            info!("Secret up to date");
-                                            continue;
-                                        }
-                                        let new = new_secret(d, &s).unwrap();
-                                        match api.create(d, &new).await {
-                                            Ok(o) => {
-                                                info!(
-                                                    "Created new secret: {}",
-                                                    client::full_name(&o)
-                                                );
-                                                // wait for it..
-                                                std::thread::sleep(
-                                                    std::time::Duration::from_millis(5_000),
-                                                );
-                                            }
-                                            Err(kube::Error::Api(ae)) => assert_eq!(ae.code, 409), // if you skipped delete, for instance
-                                            Err(e) => error!("Error {}", e),
-                                        }
-                                    }
-                                    Err(_) => {
-                                        let new = new_secret(d, &y).unwrap();
-                                        match api.create(d, &new).await {
-                                            Ok(o) => {
-                                                info!(
-                                                    "Created new secret: {}",
-                                                    client::full_name(&o)
-                                                );
-                                                // wait for it..
-                                                std::thread::sleep(
-                                                    std::time::Duration::from_millis(5_000),
-                                                );
-                                            }
-                                            Err(kube::Error::Api(ae)) => assert_eq!(ae.code, 409), // if you skipped delete, for instance
-                                            Err(e) => error!("{}", e),
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+    Controller::new(cmgs, ListParams::default())
+        .owns(cms, ListParams::default())
+        .run(reconcile, error_policy, Context::new(Data { client }))
+        .for_each(|res| async move {
+            match res {
+                Ok(o) => info!("reconciled {:?}", o),
+                Err(e) => warn!("reconcile failed: {}", Report::from(e)),
             }
-        }
-    }
+        })
+        .await;
+
     Ok(())
-}
-
-fn new_secret(full_name: &str, secret: &Secret) -> Result<Secret, Error> {
-    let (namespace, name) = client::split_full_name(full_name);
-    let so: Secret = serde_json::from_value(json!({
-        "apiVersion": "v1",
-        "kind": "Secret",
-        "metadata": {
-            "name": name,
-            "namespace": namespace,
-            "labels": {
-                LABEL: "bond",
-            }
-        },
-        "type": secret.type_,
-        "data": secret.data
-    }))?;
-    Ok(so)
-}
-
-async fn serve() {
-    let live = warp::path!("live").map(|| r#"{"status":"OK"}"#);
-
-    warp::serve(live).run(([127, 0, 0, 1], 3030)).await;
-}
-
-#[test]
-fn it_works() {
-    let mut s: Secret = Default::default();
-    let mut a = std::collections::BTreeMap::<String, String>::new();
-    a.insert(LABEL.to_string(), "bond".to_string());
-    s.metadata.name = Some("bar".to_string());
-    s.metadata.namespace = Some("foo".to_string());
-    s.metadata.labels = Some(a);
-    assert_eq!(s, new_secret("foo/bar", &s).unwrap())
 }
